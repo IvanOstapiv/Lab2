551a69145471434c42cd4e4bc1f98b4d829e209d
IvanOstapiv <65124766+IvanOstapiv@users.noreply.github.com>
2021-03-01 15:06:46 +0200

    Add files via upload

---
+++ 2_3A.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Lab2_3A
+{
+    public class Program
+    {
+
+        static public int Mi_P(int[] a, int n) //підпрограмма, яка шукає мінімальне число і його індекс;
+        {
+            int min = a[0];
+            for (int i = 0; i < n; ++i) //Шукаєм мінімальне число;
+            {
+                if (a[i] < min)
+                    min = a[i];
+            }
+            int mi = 0;
+            for (int i = 0; i < n; ++i) //Шукаєм інденкс мінімального числа;
+            {
+                if (min == a[i])
+                {
+                    mi = i;
+                }
+
+            }
+            return mi; //Повертаєм інденкс мінімального числа;
+        }
+        static public int Dob_P(int[] a, int n) //підпрограмма, яка множить числа до першого нульового елемента;
+        {
+            int dob = 1, zero1 = 998, in_zero = 999;
+            for (int i = 0; i < n; ++i)  //Шукаєм "нуль" елемент;
+            {
+                if (a[i] == 0)
+                {
+                    zero1 = a[i];
+                }
+            }
+            for (int i = 0; i < n; ++i) //Шукаєм індекс "нуль" елемента;
+            {
+                if (zero1 == a[i])
+                {
+                    in_zero = i;
+                }
+            }
+            if (zero1 == 998 && in_zero == 999) //якщо нуля немає, то прирівнюєм наш dob = 0;
+            {
+                dob = 0;
+            }
+            else //якщо є нуль то просто рахуєм добуток;
+            {
+                for (int i = 0; i < in_zero; ++i)
+                {
+                    dob *= a[i];
+                }
+            }
+            return dob; //повертаєм добуток;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть кiлькiсть елементiв масиву");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write("a[" + i + "]=");
+                    a[i] = int.Parse(Console.ReadLine()); // Вводим числа з клавіатури;
+            }
+            int mi = Mi_P(a, n); //підпрограмма, яка шукає мінімальне число і його індекс;
+            int dob = Dob_P(a, n); //підпрограмма, яка множить числа до першого нульового елемента;
+            if (dob == 0) //якщо dob = 0, то у масиві немає нулів;
+            {
+                Console.WriteLine("У масивi немає нулiв");
+            }
+            else //якщо є нуль, то виводим наш добуток;
+            {
+                Console.WriteLine("Добуток всiх елементiв до першого нульового елемента = " + dob);
+            }
+            Console.WriteLine("Номер мiнiмального елемента масиву = " + mi); //індекс мін числа;
+        }
+    }
+}
+
